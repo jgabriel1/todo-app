81018c0ba6c3ea70c69f289ef3732a6023463f84
jgabriel1 <josegabriel@det.ufc.br>
2020-04-24 19:50:51 -0300

    List of individual checkbox forms. Trying a different approach.

--- app/forms.py
+++ app/forms.py
@@ -4,11 +4,10 @@ from wtforms.validators import DataRequired
 
 
 class TaskForm(FlaskForm):
-
     description = TextField(id='new-task', validators=[DataRequired()])
     submit = SubmitField(id='submit-task', description='Submit')
 
 
 class TaskCompleteForm(FlaskForm):
-
     completed = BooleanField(id='task-completed')
+    submit = SubmitField(id='submit-completion', label='not_submit')
--- app/views.py
+++ app/views.py
@@ -8,17 +8,19 @@ from .models import db, Task
 @app.route('/', methods=['GET', 'POST'])
 def index():
     tasks_list = Task.query.all()
+    tasks_form_list = [
+        TaskCompleteForm(completed=task.completed) for task in tasks_list
+    ]
 
-    for task in tasks_list:
-        task.complete_form = TaskCompleteForm(
-            default=task.completed  # View FlaskForm docs;
-        )
-
-        if task.complete_form.validate_on_submit():
+    for i, task in enumerate(tasks_list):
+        if tasks_form_list[i].is_submitted():
             task_model_instance = Task.query.filter_by(
                 description=task.description
             ).first()
+
             task_model_instance.completed = not task_model_instance.completed
+            task_model_instance.finished_at = datetime.utcnow()
+
             db.session.commit()
 
     form = TaskForm()
@@ -36,4 +38,9 @@ def index():
 
         return redirect('/')
 
-    return render_template('index.html', form=form, tasks_list=tasks_list)
+    return render_template(
+        'index.html',
+        form=form,
+        tasks_list=tasks_list,
+        tasks_form_list=tasks_form_list
+    )
